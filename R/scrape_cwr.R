e12c154514baf7b68b8ba7d26ebafb1986428458
jadeynryan <jadey.nicole.ryan@gmail.com>
2023-04-08 15:55:22 -0700

    Work on `get_encounter_data()` and `parse_encounter()`. Add `make_encounter_df()`.

--- R/scrape_cwr.R
+++ R/scrape_cwr.R
@@ -1,4 +1,4 @@
-#' Extract Link for Each Encounter
+#' Extract Link for Each CWR Encounter
 #' @description Extract encounter links from
 #'   \href{https://www.whaleresearch.com/}{Center for Whale
 #'   Research (CWR)} encounter landing pages.
@@ -13,18 +13,18 @@
 #'   Defaults to "current".
 #' @param max_urls Number of urls to extract. Defaults to `Inf`.
 #'
-#' @returns Character vector of urls
+#' @returns Character vector of urls.
 #' @export
 #'
 #' @examples
-#' library(rvest)
 #' get_encounter_links("https://www.whaleresearch.com/2022encounters",
 #'                     max_urls = 10)
 get_encounter_links <-
   function(landing_url = "https://www.whaleresearch.com/2022encounters",
            website_type = "current",
            max_urls = Inf) {
-    stopifnot("landing_url must be a character string" = is.character(landing_url))
+    stopifnot("landing_url must be a character string" =
+                is.character(landing_url))
     stopifnot("max_urls must be numeric." = is.numeric(max_urls))
 
     website_type <- match.arg(website_type, c("current", "archive"))
@@ -39,32 +39,116 @@ get_encounter_links <-
       utils::head(max_urls)
   }
 
-# # Extract data from encounter
-# url <- "https://www.whaleresearch.com/2023-13"
-#
-# encounter <- read_html(url) |>
-#   html_elements("#comp-lfqwu3ih2 span") |>
-#   html_text2()
-#
-# get_encounter_data <- function(encounter_url) {
-#   read_html(encounter_url) |>
-#     html_elements("#comp-lfqwu3ih2 span") |>
-#     html_text2()
-# }
-#
-# encounter <- as.data.frame(encounter) |>
-#   # Separate column by first colon
-#   separate(
-#     encounter,
-#     into = c("variable", "data"),
-#     sep = ":",
-#     extra = "merge"
-#   ) |>
-#   # Move encounter summary text (row 15) to data column
-#   mutate(data = case_when(variable == "EncSummary" ~ variable[15],
-#                           TRUE ~ data)) |>
-#   # Select the 14 rows with data (removes extra EncSummary row)
-#   head(14) |>
-#   # Pivot wider to tidy dataframe
-#   pivot_wider(names_from = "variable",
-#               values_from = "data")
+#' Extract CWR Data from Encounter Webpage
+#'
+#' @param encounter_url Character url for CWR encounter.
+#'
+#' @returns Character vector to be input to `parse_encounter()`.
+#' @export
+#'
+#' @examples
+#' get_encounter_data("https://www.whaleresearch.com/2023-13")
+get_encounter_data <- function(encounter_url) {
+  # Be polite and wait 3 seconds to avoid overloading server
+  Sys.sleep(3)
+
+  # Scrape webpage
+  results <- rvest::read_html(encounter_url) |>
+    rvest::html_elements(xpath = "//*[starts-with(@id,'comp-l')]//p") |>
+    rvest::html_text2()
+
+  # Print message for which url is being read
+  message("Scraped URL: ", encounter_url)
+
+  return(results)
+}
+
+#' Parse CWR Encounter Data
+#'
+#' @param encounter_data Character vector from `get_encounter_data()`.
+#'
+#' @returns Dataframe with one row of encounter data parsed into columns.
+#' @export
+#'
+#' @examples
+#' get_encounter_data("https://www.whaleresearch.com/2023-13") |>
+#' parse_encounter()
+parse_encounter <- function(encounter_data) {
+  # Text to remove from dataframe
+  remove <- paste(c("Photos taken",
+                    "Get the latest",
+                    "Join our email list"),
+                  collapse = "|")
+
+  results <- as.data.frame(encounter_data) |>
+    # Filter rows that are empty or irrelevant
+    dplyr::filter(!grepl(remove, encounter_data) &
+                    !encounter_data == " ") |>
+    # Move NMFS permit to top row
+    dplyr::arrange(!grepl("NMFS", encounter_data))
+
+  # Get row index of Encounter Summary
+  summary_row_index <- which(grepl("EncSummary", encounter_data))
+
+  # Make one row dataframe of encounter summary (useful since some
+  # encounter summaries span multiple list elements)
+  encounter_summary <- results |>
+    dplyr::slice(summary_row_index:dplyr::n()) |>
+    dplyr::summarize(encounter_data = paste(encounter_data,
+                                            collapse = ""))
+
+  results <- results |>
+    # Remove multi-row encounter summary
+    dplyr::slice(1:summary_row_index - 1) |>
+    # Add single row encounter summary
+    dplyr::bind_rows(encounter_summary) |>
+    # Separate column by first colon
+    tidyr::separate(
+      encounter_data,
+      into = c("variable", "data"),
+      sep = ":",
+      extra = "merge"
+    ) |>
+    # Trim whitespace
+    dplyr::mutate(data = trimws(.data$data)) |>
+    # Pivot wider to tidy dataframe
+    tidyr::pivot_wider(names_from = "variable",
+                       values_from = "data")
+
+  # Clean column names
+  names(results) <- janitor::make_clean_names(names(results))
+
+  # Convert column types
+  results <- results |>
+    dplyr::mutate(
+      enc_date = as.Date(.data$enc_date,
+                         tryFormats = c("%d/%m/%y", "%d-%m-%y")),
+      enc_seq = as.integer(.data$enc_seq),
+      enc_number = as.integer(.data$enc_number)
+    )
+
+  return(results)
+}
+
+#' Make a Dataframe of Multiple CWR Encounters
+#'
+#' @param encounter_urls Character vector of CWR urls. Pipe
+#'   `get_encounter_links` to this function.
+#'
+#' @returns Dataframe with one row per encounter.
+#' @export
+#'
+#' @examples
+#' # Piping the `get_encounter_links()` function
+#' get_encounter_links(max_urls = 3) |>
+#'   make_encounter_df()
+#'
+#' # Providing a character vector of urls
+#' make_encounter_df(c("https://www.whaleresearch.com/2023-4",
+#'                     "https://www.whaleresearch.com/2023-09"))
+make_encounter_df <- function(encounter_urls) {
+  as.list(encounter_urls) |>
+    purrr::map(.x = _, get_encounter_data) |>
+    purrr::map(.x = _, parse_encounter) |>
+    dplyr::bind_rows()
+}
--- tests/testthat/test-scrape_cwr.R
+++ tests/testthat/test-scrape_cwr.R
@@ -1,18 +1,32 @@
 test_that("get_encounter_links() works", {
-  # Error if max_urls is not numeric
-  expect_error(
-    get_encounter_links("https://www.whaleresearch.com/2022encounters",
-                        max_urls = "a")
+  # Returns character vector
+  expect_type(
+    get_encounter_links("https://www.whaleresearch.com/2022encounters"),
+    "character"
   )
-  # Max urls = 3 has vector with length 3
+
+  # Default max_urls (Inf) captures all links and archive website_type works
+  expect_length(
+    get_encounter_links(
+      "https://whaleresearch.wixsite.com/2021encounters",
+      website_type = "archive"
+    ),
+    89
+  )
+
+  # max_urls = 3 has vector with length 3
   expect_length(
     get_encounter_links("https://www.whaleresearch.com/2022encounters",
                         max_urls = 3),
     3
   )
-  # Returns character vector
-  expect_type(
-    get_encounter_links("https://www.whaleresearch.com/2022encounters"),
-    "character"
+
+  # Error if url is not character
+  expect_error(get_encounter_links(2))
+
+  # Error if max_urls is not numeric
+  expect_error(
+    get_encounter_links("https://www.whaleresearch.com/2022encounters",
+                        max_urls = "a")
   )
 })
